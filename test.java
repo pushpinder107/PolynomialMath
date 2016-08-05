0457bbeb180891a74efaff635f50c43c2a2c7dda
Naveen Raja <tabotnaveen@gmail.com>
2016-08-05 09:36:04 +0530

    Update test.java
    Display and Multiplication Has to be done. It fails for X power 1 and constant.

--- test.java
+++ test.java
@@ -1,6 +1,8 @@
 import java.util.*;
 import java.lang.*;
 import java.io.*;
+import java.util.regex.*;
+
 
 class Polynomial
 {
@@ -11,10 +13,14 @@ class Polynomial
     public Polynomial(){
         polynomialMap = new HashMap<>();
     }
-
+    
     public Polynomial(String input){
         polynomialMap = new HashMap<>();
         parseStringToPoly(input);
+    }
+    
+    public Polynomial(HashMap<Integer,Integer> map){
+    	polynomialMap.putAll(map);
     }
     
     private Polynomial parseStringToPoly(String polyString){
@@ -34,13 +40,20 @@ class Polynomial
 			elementSet = element.split("[a-z]");
 			coefficient = Integer.parseInt(elementSet[0]);
 			exponent = Integer.parseInt(elementSet[1]);
-			this.polynomialMap.put(coefficient, exponent);
+			this.polynomialMap.put(exponent, coefficient);
 		}
 		return this;
 	}
-    
+   
+  /*  
     public String toString(){
-    
+        for (polynomialMap.Entry<Integer, Integer> entry : polynomialMap.entrySet())
+        {
+            System.out.println(entry.getKey() + "/" + entry.getValue());
+        }
+        
+    }
+
         
     }
     
@@ -51,7 +64,7 @@ class Polynomial
     public String toLatex(){
         
     }
-    
+    */
     public HashMap<Integer,Integer> getPMap(){
         return polynomialMap;
     }
@@ -67,17 +80,56 @@ class PolynomialArithmetic{
         result = new Polynomial();
     }
     
-    public Polynomial addTwo(Polynomial p1, Polynomial p2){
-        
-    }
     
-    public Polynomial subtractTwo(Polynomial p1, Polynomial p2){
-        
-    }
+    public Polynomial addTwo(Polynomial A, Polynomial B){
+    		HashMap<Integer,Integer> sumMap = new HashMap<Integer,Integer>();
+    		HashMap<Integer,Integer> AMap = A.getPMap();
+    		HashMap<Integer,Integer> BMap = B.getPMap();
+    		
+    		sumMap.putAll(AMap);
+    		
+    		for (Map.Entry<Integer, Integer> entry : BMap.entrySet()) {
+    		    if(sumMap.containsKey(entry.getKey())){
+    		    	sumMap.put(entry.getKey(), sumMap.get(entry.getKey())+entry.getValue());
+    		    }
+    		    else
+    		    	sumMap.put(entry.getKey(), entry.getValue());
+    			//System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
+    		}
+    		
+    		Polynomial sum=new Polynomial(sumMap);
+    		
+    		for (Map.Entry<Integer, Integer> entry : sumMap.entrySet()) {
+    		   System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
+    		}
+    		return sum;
+    } 
     
-    public Polynomial multiplyTwo(Polynomial p1, Polynomial p2){
+    
+    public Polynomial subtractTwo(Polynomial A, Polynomial B){
+    	HashMap<Integer,Integer> diffMap = new HashMap<Integer,Integer>();
+		HashMap<Integer,Integer> AMap = A.getPMap();
+		HashMap<Integer,Integer> BMap = B.getPMap();
+		
+		diffMap.putAll(AMap);
+		
+		for (Map.Entry<Integer, Integer> entry : BMap.entrySet()) {
+		    if(diffMap.containsKey(entry.getKey())){
+		    	diffMap.put(entry.getKey(), diffMap.get(entry.getKey())+entry.getValue());
+		    }
+		    else
+		    	diffMap.put(entry.getKey(), entry.getValue());
+			//System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
+		}
+		
+		Polynomial diff=new Polynomial(diffMap);
+		return diff;
+    }
+ /*   
+    public Polynomial multiplyTwo(Polynomial A, Polynomial B){
         
     }
+   */
 }
 
 class Test {
@@ -85,11 +137,13 @@ class Test {
     public static void main (String[] args) throws java.lang.Exception
 	{
 	    PolynomialArithmetic op = new PolynomialArithmetic();
-	    Polynomial p1 = new Polynomial("");
-	    Polynomial p2 = new Polynomial("");
-	    p1.addTwo(p1,p2);
-	    p2.subtractTwo(p1,p2);
+	    Polynomial p1 = new Polynomial("3x2");
+	    Polynomial p2 = new Polynomial("4x2");
+	    op.addTwo(p1,p2);
+	    op.subtractTwo(p1,p2);
 	    
 		
 	}
 }
+
+
